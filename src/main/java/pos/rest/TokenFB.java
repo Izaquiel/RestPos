6106252c9aabead2eb3b9fa6474aca779456a16e
Izaquiel <Izaquiel@IzaquielCruz>
2015-03-08 00:51:20 -0300

    Publicando no face OK com o postman, falta marcar os usuarios.

--- src/main/java/pos/rest/TokenFB.java
+++ src/main/java/pos/rest/TokenFB.java
@@ -2,7 +2,7 @@ package pos.rest;
 
 public class TokenFB {
 
-    private String Token = "CAAVvydN5IE4BAI1Cqc3nvZBBi2d5eA5tiULEwa4TRvhBwAs5NyBoCpe2AJMPm40rxF8RRGmLVYAG0llpPhE9PegruMiwHH7fjKaP1nYZB80wMwFmx25dzukAToAH4pOZAhAg885AU8zjKUNdJBjQm0RCa28ZAMoxykXbbhSvdFEuZCee7hNLyt7YNl7YCJm38HZClsemgMM00ZAo6fSct9Q";
+    private String Token = "";
 
     public String getToken() {
         return Token;
